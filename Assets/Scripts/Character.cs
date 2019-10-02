688c96d1d9de205c8f2973c88f97910743c9ca2c
Pufan Zhao <geoff.zhao@mail.utoronto.ca>
2019-10-01 23:29:13 -0400

    add path creator

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,17 @@
+﻿using PathCreation;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 20;
+    float distanceTravelled;
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+    }
+}
