f4ab720b4a49edac0c69fd558cacc0079f56177d
Xinyi Gong <x.gong@ibm.com>
2019-10-08 21:33:13 -0400

    add misuc

--- Assets/Scripts/Theme_sound.cs
+++ Assets/Scripts/Theme_sound.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class Theme_sound : MonoBehaviour
 {
     // Start is called before the first frame update
-    //public AudioClip MusicClip;
-    //public AudioSource MusicSource;
-
-
-
-    //private void Start()
-    //{
+    // public AudioClip MusicClip;
+    // public AudioSource MusicSource;
+    // private void Start()
+    // {
 
     //    MusicSource.clip = MusicClip;
     //    MusicSource.Play();
-    //}
+    // }
 
 }
